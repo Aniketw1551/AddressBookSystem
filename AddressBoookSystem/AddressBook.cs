ca5fb2c029db2c841e285befc28e3adfd2929920
Aniketw1551 <aniketwaghmare976@gmail.com>
2022-01-22 20:35:33 +0530

    [Aniket] Adding AddContact program

--- AddressBoookSystem/AddressBook.cs
+++ AddressBoookSystem/AddressBook.cs
@@ -21,10 +21,31 @@ namespace AddressBoookSystem
 
         public void Contact()  //Method to contact 
         {
-            Console.WriteLine("FirstName: " + this.FirstName  + " LastName: " + this.LastName 
-                              + " Address: " + this.Address  + " City: " + this.City  + " State: "
-                              + this.State  + " Zip: " + this.Zip  + " PhoneNumber: "
-                              + this.PhoneNumber  + " Email: " + this.Email);
+            Console.WriteLine("FirstName: " + this.FirstName + " LastName: " + this.LastName
+                              + " Address: " + this.Address + " City: " + this.City + " State: "
+                              + this.State + " Zip: " + this.Zip + " PhoneNumber: "
+                              + this.PhoneNumber + " Email: " + this.Email);
+        }
+
+        public void AddContact()  //Method to Add Contact in a List
+        {
+            Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
+            AddressBook addressBook = new AddressBook()  //Initializing elements using collection-initializer syntax
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToDouble(Console.ReadLine()),
+                PhoneNumber = Convert.ToDouble(Console.ReadLine()),
+                Email = Console.ReadLine(),
+            };
+
+            //creating a List to store contacts in List
+            IList<AddressBook> AddressBookList = new List<AddressBook>();  //created a List
+            AddressBookList.Add(addressBook);
+            addressBook.Contact();
         }
     }
 }
--- AddressBoookSystem/Program.cs
+++ AddressBoookSystem/Program.cs
@@ -11,21 +11,9 @@ namespace AddressBoookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to AddressBookSystem program");
-            
-            Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
-            AddressBook addressBook = new AddressBook()  //Initializing elements using collection-initializer syntax
 
-            {
-                FirstName = Console.ReadLine(),
-                LastName = Console.ReadLine(),
-                Address = Console.ReadLine(),
-                City = Console.ReadLine(),
-                State = Console.ReadLine(),
-                Zip = Convert.ToDouble(Console.ReadLine()),
-                PhoneNumber = Convert.ToDouble(Console.ReadLine()),
-                Email = Console.ReadLine(),
-            };
-            addressBook.Contact();
+            AddressBook addressBook = new AddressBook();
+            addressBook.AddContact();
             Console.ReadLine();
         }
     }
